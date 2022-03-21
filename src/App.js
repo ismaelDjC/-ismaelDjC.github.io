3a412620832302bf3e9c4cef50c11d4a0777d9d6
jefferson Condo <jefferson.condo.est@tecazuay.edu.ec>
2022-03-21 13:45:02 -0500

    Commit base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import ProductContextProvider from './contexts/ProductContext';
+import ProductList from './componets/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductContextProvider>
+        <ProductList/>
+      </ProductContextProvider>
     </div>
   );
 }
